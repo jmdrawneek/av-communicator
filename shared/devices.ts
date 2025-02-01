c5c1497326138b2b95f2c2d9d6b7bd204b4e868b
jmdrawneek <james@drawneek.io>
2025-02-01 20:15:28 +0000

    chore: update device action examples

--- shared/devices.ts
+++ shared/devices.ts
@@ -1,26 +1,32 @@
 export const deviceList = {
-    lightModel1: {
+    Sony_Light_XLMK2ef: {
         name: 'Light 1 Model 1',
+        manufacturer: 'Sony',
+        model: 'XLMK2ef',
         type: 'light',
         actions: [
-            {id: 'powerOn', label: 'Power On'}, 
-            {id: 'powerOff', label: 'Power Off'}, 
-            {id: 'changeBrightness', label: 'Change Brightness'}
+            { id: '11111111111', label: 'Power On' }, 
+            { id: '22222222222', label: 'Power Off' }, 
+            { id: '33333333333', label: 'Change Brightness' }
         ]
     },
-    screenModel1: {
+    Sony_Screen_FTG54d: {
         name: 'Screen 1 Model 1',
+        manufacturer: 'Sony',
+        model: 'FTG54d',
         type: 'screen',
         actions: [
-            {id: 'powerOn', label: 'Power On'}, 
-            {id: 'powerOff', label: 'Power Off'}
+            { id: '44444444444', label: 'Power On' }, 
+            { id: '55555555555', label: 'Power Off' }
         ]
     },
-    hdmiSwitchModel1: {
+    Sony_HDMI_Switch_UDNGHF: {
         name: 'HDMI Switch 1 Model 1',
+        manufacturer: 'Sony',
+        model: 'UDNGHF',
         type: 'hdmiSwitch',
         actions: [
-            {id: 'switchInput', label: 'Switch Input'}
+            { id: '66666666666', label: 'Switch Input' }
         ]
     }
 }
@@ -29,10 +35,36 @@ export type Device = typeof deviceList[keyof typeof deviceList];
 
 
 export const deviceCommands = {
-    'lightModel1': {
-        powerOn: {
-            command: 'beepBoop',
-            return: 'beepBoopBob'
-        }
+    '11111111111': {
+        supportedModels: ['XLMK2ef'],
+        command: 'beepBoop',
+        return: 'beepBoopBob'
+    },
+    '22222222222': {
+        supportedModels: ['XLMK2ef'],
+        command: 'beepBoop',
+        return: 'beepBoopBob'
+    },
+    '33333333333': {
+        supportedModels: ['XLMK2ef'],
+        command: 'beepBoop',
+        return: 'beepBoopBob'
+    },
+    '44444444444': {
+        supportedModels: ['FTG54d'],
+        command: 'beepBoop',
+        return: 'beepBoopBob'
+    },
+    '55555555555': {
+        supportedModels: ['FTG54d'],
+        command: 'beepBoop',
+        return: 'beepBoopBob'
+    },
+    '66666666666': {
+        supportedModels: ['UDNGHF'],
+        command: 'beepBoop',
+        return: 'beepBoopBob'
     }
 }
+
+export type DeviceCommand = typeof deviceCommands[keyof typeof deviceCommands];
